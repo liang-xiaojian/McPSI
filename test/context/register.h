c563fc614a778dccfbbd971f5eafa870726a1a63
liang-xiaojian <56351491+liang-xiaojian@users.noreply.github.com>
2023-10-24 22:27:23 +0800

    Update register.h

---
+++ test/context/register.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "test/context/context.h"
+#include "test/context/state.h"
+#include "test/cr/fake_cr.h"
+#include "test/ss/protocol.h"
+
+namespace test {
+
+void inline InitContext(std::shared_ptr<Context> ctx) {
+  uint128_t seed = ctx->SyncSeed();
+  ctx->states_->InsertState<Prg>(seed);
+  ctx->states_->InsertState<Protocol>(ctx);
+  auto key = ctx->GetState<Protocol>()->GetKey();
+  ctx->states_->InsertState<FakeCorrelation>(ctx);
+  ctx->GetState<FakeCorrelation>()->SetKey(key);
+}
+
+void inline MockInitContext(std::vector<std::shared_ptr<Context>>& ctxs) {
+  YACL_ENFORCE(ctxs.size() == 2);
+  auto task0 = std::async([&] { InitContext(ctxs[0]); });
+  auto task1 = std::async([&] { InitContext(ctxs[1]); });
+  task0.get();
+  task1.get();
+}
+
+}  // namespace test
