464de94cf2fe42e260f451b4e67461fe59e71ff2
liangxiaojian <im.liangxj@gmail.com>
2023-11-13 20:48:33 +0800

    add wolverine VOLE

--- mcpsi/cr/utils/vole_test.cc
+++ mcpsi/cr/utils/vole_test.cc
@@ -8,6 +8,7 @@
 #include "mcpsi/cr/cr.h"
 #include "mcpsi/utils/test_util.h"
 #include "mcpsi/utils/vec_op.h"
+#include "vole.h"
 #include "yacl/crypto/utils/rand.h"
 
 namespace mcpsi::vole {
@@ -64,14 +65,106 @@ TEST_P(MpVoleTest, Work) {
   size_t i = 0;
   size_t j = 0;
   for (; i < mp_vole_size && j < noise_num; ++i) {
-    if (s_output[i] + r_output[i] != internal::PTy(0)) {
-      EXPECT_EQ(s_output[i] + r_output[i], v[j] + w[j]);
+    if (s_output[i] != r_output[i]) {
+      EXPECT_EQ(s_output[i] - r_output[i], w[j] - v[j]);
       EXPECT_TRUE(indexes.count(i));
       ++j;
     }
   }
   for (; i < mp_vole_size; ++i) {
-    EXPECT_EQ(s_output[i] + r_output[i], internal::PTy(0));
+    EXPECT_EQ(s_output[i], r_output[i]);
+  }
+}
+
+TEST(WolverineVoleTest, PreWork) {
+  auto context = MockContext(2);
+  MockInitContext(context);
+
+  auto param = LpnParam::GetPreDefault();
+  param.mp_param_.GenIndexes();
+  auto cot = yc::MockCots(param.mp_param_.require_ot_num_, yc::RandU128());
+  size_t pre_num = param.k_;
+  size_t vole_num = param.n_;
+
+  auto pre_a = Rand(pre_num);
+  auto pre_b = Rand(pre_num);
+  auto deltas = Rand(1);
+  auto delta = deltas[0];
+
+  auto pre_c = ScalarMul(delta, absl::MakeSpan(pre_a));
+  Add(absl::MakeConstSpan(pre_c), absl::MakeConstSpan(pre_b),
+      absl::MakeSpan(pre_c));
+
+  auto rank0 = std::async([&] {
+    auto cr = context[0]->GetState<Correlation>();
+    auto conn = context[0]->GetConnection();
+    std::vector<internal::PTy> c(vole_num, 0);
+    WolverineVoleSend(conn, cot.send, param, absl::MakeSpan(pre_c),
+                      absl::MakeSpan(c));
+    return c;
+  });
+  auto rank1 = std::async([&] {
+    auto cr = context[1]->GetState<Correlation>();
+    auto conn = context[1]->GetConnection();
+    std::vector<internal::PTy> a(vole_num, 0);
+    std::vector<internal::PTy> b(vole_num, 0);
+    WolverineVoleRecv(conn, cot.recv, param, absl::MakeSpan(pre_a),
+                      absl::MakeSpan(pre_b), absl::MakeSpan(a),
+                      absl::MakeSpan(b));
+    return std::make_pair(a, b);
+  });
+
+  auto c = rank0.get();
+  auto [a, b] = rank1.get();
+
+  for (size_t i = 0; i < vole_num; ++i) {
+    EXPECT_EQ(a[i] * delta + b[i], c[i]);
+  }
+}
+
+TEST(WolverineVoleTest, BootStrapWork) {
+  auto context = MockContext(2);
+  MockInitContext(context);
+
+  auto param = LpnParam::GetDefault();
+  param.mp_param_.GenIndexes();
+  auto cot = yc::MockCots(param.mp_param_.require_ot_num_, yc::RandU128());
+  size_t pre_num = param.k_;
+  size_t vole_num = param.n_;
+
+  auto pre_a = Rand(pre_num);
+  auto pre_b = Rand(pre_num);
+  auto deltas = Rand(1);
+  auto delta = deltas[0];
+
+  auto pre_c = ScalarMul(delta, absl::MakeSpan(pre_a));
+  Add(absl::MakeConstSpan(pre_c), absl::MakeConstSpan(pre_b),
+      absl::MakeSpan(pre_c));
+
+  auto rank0 = std::async([&] {
+    auto cr = context[0]->GetState<Correlation>();
+    auto conn = context[0]->GetConnection();
+    std::vector<internal::PTy> c(vole_num, 0);
+    WolverineVoleSend(conn, cot.send, param, absl::MakeSpan(pre_c),
+                      absl::MakeSpan(c));
+    return c;
+  });
+  auto rank1 = std::async([&] {
+    auto cr = context[1]->GetState<Correlation>();
+    auto conn = context[1]->GetConnection();
+    std::vector<internal::PTy> a(vole_num, 0);
+    std::vector<internal::PTy> b(vole_num, 0);
+    WolverineVoleRecv(conn, cot.recv, param, absl::MakeSpan(pre_a),
+                      absl::MakeSpan(pre_b), absl::MakeSpan(a),
+                      absl::MakeSpan(b));
+    return std::make_pair(a, b);
+  });
+
+  auto c = rank0.get();
+  auto [a, b] = rank1.get();
+
+  for (size_t i = 0; i < vole_num; ++i) {
+    EXPECT_EQ(a[i] * delta + b[i], c[i]);
   }
 }
 
